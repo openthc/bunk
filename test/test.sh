e8f14717baad61592fb6f55f20425f78e21fd2cc
openthc <code@openthc.com>
2021-02-10 17:53:30 +0000

    test updates, sync test.sh

--- test/test.sh
+++ test/test.sh
@@ -3,58 +3,56 @@
 # OpenTHC Test Runner
 #
 
-#set -o errexit
+set -o errexit
 set -o nounset
 # set -o pipefail
 
 f=$(readlink -f "$0")
 d=$(dirname "$f")
-dt=$(date)
 
 cd "$d"
 
-output_path="../webroot/test-output"
-if [ ! -d "$output_path" ]
-then
-	mkdir "$output_path"
-fi
-
-output_main="$output_path/index.html"
-
-echo '<h1>Tests Started</h1>' > "$output_main"
+output_base="../webroot/test-output"
+output_main="$output_base/index.html"
+mkdir -p "$output_base"
 
 
 #
+# Lint
+echo '<h1>Linting...</h1>' > "$output_main"
+find ../api/ ../bin/ ../lib/ ../sbin/ ../view/ -type f -name '*.php' -exec php -l {} \; \
+	| grep -v 'No syntax' || true \
+	2>&1 >"$output_base/phplint.txt"
+
+
 #
+# PHPUnit
+echo '<h1>Running Tests...</h1>' > "$output_main"
 ../vendor/bin/phpunit \
-	"$@" 2>&1 | tee "$output_path/output.txt"
-	#--log-junit "$output_path/output.xml" \
-	#--testdox-html "$output_path/testdox.html" \
-	#--testdox-text "$output_path/testdox.txt" \
-	#--testdox-xml "$output_path/testdox.xml" \
-	#--verbose \
+	--verbose \
+	--log-junit "$output_base/output.xml" \
+	--testdox-html "$output_base/testdox.html" \
+	--testdox-text "$output_base/testdox.txt" \
+	--testdox-xml "$output_base/testdox.xml" \
+	"$@" 2>&1 | tee "$output_base/output.txt" || true
 
-# if [[ $ret != 0 ]]
-# then
-# 	echo "PHPUnit Failed"
-# 	exit 1;
-# fi
-note=$(tail -n1 "$output_path/output.txt")
-
-echo '<h1>Tests Completed</h1>' > "$output_main"
 
 #
-# Get Transform
+# Transform
 echo '<h1>Transforming...</h1>' > "$output_main"
-curl -qs https://openthc.com/pub/phpunit/report.xsl > report.xsl
+[ -f "report.xsl" ] || wget -q 'https://openthc.com/pub/phpunit/report.xsl'
 xsltproc \
 	--nomkdir \
-	--output "$output_path/output.html" \
+	--output "$output_base/output.html" \
 	report.xsl \
-	"$output_path/output.xml"
+	"$output_base/output.xml"
+
 
 #
-# Final Ouptut
+# Final Output
+dt=$(date)
+note=$(tail -n1 "$output_base/output.txt")
+
 cat <<HTML > "$output_main"
 <html>
 <head>
@@ -62,14 +60,14 @@ cat <<HTML > "$output_main"
 <meta name="viewport" content="initial-scale=1, user-scalable=yes">
 <meta name="theme-color" content="#247420">
 <link rel="stylesheet" href="https://cdn.openthc.com/bootstrap/4.4.1/bootstrap.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous">
-<title>Test Result $dt</title>
+<title>Test Result ${dt}</title>
 </head>
 <body>
 <div class="container mt-4">
 <div class="jumbotron">
 
-<h1>Test Result $dt</h1>
-<h2>$note</h2>
+<h1>Test Result ${dt}</h1>
+<h2>${note}</h2>
 
 <p>You can view the <a href="output.txt">raw script output</a>,
 or the <a href="output.xml">Unit Test XML</a>
