1f87b186f9c94e68a57201ea012eeb67607f0dea
Timothy Brantley II <tbrantleyii@gmail.com>
2021-03-23 13:30:07 -0400

    fix: bad boot.php

--- test/boot.php
+++ test/boot.php
@@ -4,7 +4,7 @@
  */
 
 // Load App bootstrap file
-require_once(dirname(dirname(__FILE__)) . '/boot.php');
+require_once(dirname(__FILE__) . '/boot.php');
 
 require_once(__DIR__ . '/Base_Test.php');
 require_once(__DIR__ . '/BioTrack_Test.php');
