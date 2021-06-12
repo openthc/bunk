ec03d0bd01e16414253a5502c1c4af125f29ae23
openthc <code@openthc.com>
2021-06-12 22:51:01 +0000

    Cleanup TestCase

--- test/boot.php
+++ test/boot.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Test Bootstrap
+ * PHP Unit Test Bootstrap
  */
 
 // Load App bootstrap file
-require_once(dirname(__FILE__) . '/boot.php');
+require_once(dirname(dirname(__FILE__)) . '/boot.php');
 
-require_once(__DIR__ . '/Base_Test.php');
+require_once(__DIR__ . '/Base_Case.php');
 require_once(__DIR__ . '/BioTrack_Test.php');
 require_once(__DIR__ . '/LeafData_Test.php');
 require_once(__DIR__ . '/Metrc_Test.php');
