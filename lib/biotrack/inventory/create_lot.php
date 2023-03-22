4067d07cc2604aa1c2fb66188ffc4ee5c75bfd74
openthc <code@openthc.com>
2023-03-22 18:29:07 +0000

    fix some biotrack handlers

--- lib/biotrack/inventory/create_lot.php
+++ lib/biotrack/inventory/create_lot.php
@@ -3,8 +3,26 @@
  * SPDX-License-Identifier: MIT
  */
 
-return $RES->withJSON(array(
-	'success' => 0,
-	'error' => 'Your session has expired.',
-	'errorcode' => 63,
-));
+use \OpenTHC\Bunk\BioTrack\Base;
+
+// return $RES->withJSON(array(
+// 	'success' => 0,
+// 	'error' => 'Your session has expired.',
+// 	'errorcode' => 63,
+// ));
+
+// return $RES->withJSON(array(
+// 	'success' => 0,
+// 	'error' => 'You cannot create flower lots greater than 15 pounds.',
+// 	'errorcode' => 202,
+// ));
+
+$ret = array(
+	'success' => '1',
+	'transactionid' => Base::_rnd_transaction_id(),
+	'sessiontime' => $time,
+	'barcode_id' => Base::_rnd_barcode(),
+	'barcode_type' => 13,
+);
+
+return $RES->withJSON($ret);
