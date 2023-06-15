e4ed6944933a3bb6ac507ae7ab39284b9c5830ae
Matt Walther <matt@mashio.net>
2023-06-14 17:21:49 -0700

    [BT] app#1568

--- lib/biotrack/sync/inventory_transfer_inbound.php
+++ lib/biotrack/sync/inventory_transfer_inbound.php
@@ -7,6 +7,7 @@
 
 use \OpenTHC\Bunk\BioTrack\Base;
 
+$inventory_transfer_inbound = array();
 
 $transfer_incoming = array(
 	"deleted" => "1",
@@ -25,9 +26,66 @@ $transfer_incoming = array(
 	"transactionid" => "4919",
 	"transactionid_original" => "4918",
 );
+$inventory_transfer_inbound[] = $transfer_incoming;
 
+// app#1568
+$transfer_incoming = array(
+	'deleted' => 0,
+	'inventoryid' => '2023164042000000',
+	'inventorytype' => 7,
+	'is_refund' => 0,
+	'location' => '990001',
+	'manifest_stop' => '1',
+	'manifestid' => '2023164090000000',
+	'outbound_license' => '999000002',
+	'price' => '0.01',
+	'quantity' => '100',
+	'refund_amount' => NULL,
+	'sessiontime' => $time,
+	'strain' => 'blue dream',
+	'transactionid' => Base::_rnd_transaction_id(),
+	'transactionid_original' => Base::_rnd_transaction_id(),
+);
+$inventory_transfer_inbound[] = $transfer_incoming;
+$transfer_incoming = array(
+	'deleted' => 0,
+	'inventoryid' => '2023164042000001',
+	'inventorytype' => 7,
+	'is_refund' => 0,
+	'location' => '990001',
+	'manifest_stop' => '1',
+	'manifestid' => '2023164090000000',
+	'outbound_license' => '999000002',
+	'price' => '0.01',
+	'quantity' => '100',
+	'refund_amount' => NULL,
+	'sessiontime' => $time,
+	'strain' => 'Shady Apples',
+	'transactionid' => Base::_rnd_transaction_id(),
+	'transactionid_original' => Base::_rnd_transaction_id(),
+);
+$inventory_transfer_inbound[] = $transfer_incoming;
+$transfer_incoming = array(
+	'deleted' => 0,
+	'inventoryid' => '2023164042000002',
+	'inventorytype' => 7,
+	'is_refund' => 0,
+	'location' => '990001',
+	'manifest_stop' => '1',
+	'manifestid' => '2023164090000000',
+	'outbound_license' => '999000002',
+	'price' => '0.01',
+	'quantity' => '100',
+	'refund_amount' => NULL,
+	'sessiontime' => $time,
+	'strain' => 'Shady Oranges',
+	'transactionid' => Base::_rnd_transaction_id(),
+	'transactionid_original' => Base::_rnd_transaction_id(),
+);
+$inventory_transfer_inbound[] = $transfer_incoming;
 
 return $RES->withJSON(array(
 	'success' => 1,
 	'inventory_transfer_inbound' => array($transfer_incoming),
+	'inventory_transfer_inbound' => $inventory_transfer_inbound,
 ));
