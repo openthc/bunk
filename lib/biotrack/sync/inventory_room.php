7580162ddd1fe20dfaefc1e8a6d114a66ed892e1
Matt Walther <matt@mashio.net>
2024-03-06 16:04:39 -0800

    cleanup

--- lib/biotrack/sync/inventory_room.php
+++ lib/biotrack/sync/inventory_room.php
@@ -5,26 +5,28 @@
 
 use \OpenTHC\Bunk\BioTrack\Base;
 
+$ret = array(
+	array(
+		"deleted" => "0",
+		"location" => "18750",
+		"name" => "Quarantine",
+		"quarantine" => "1",
+		"roomid" => "1",
+		"transactionid" => "4032",
+		"transactionid_original" => "4032"
+	),
+	array(
+		"deleted" => "0",
+		"location" => "18750",
+		"name" => "New",
+		"quarantine" => "0",
+		"roomid" => "7",
+		"transactionid" => "4057",
+		"transactionid_original" => "4057"
+	),
+);
+
 return $RES->withJSON(array(
 	'success' => 1,
-	'inventory_room' => array(
-		0 => array(
-			"deleted" => "0",
-			"location" => "18750",
-			"name" => "Quarantine",
-			"quarantine" => "1",
-			"roomid" => "1",
-			"transactionid" => "4032",
-			"transactionid_original" => "4032"
-		),
-		1 => array(
-			"deleted" => "0",
-			"location" => "18750",
-			"name" => "New",
-			"quarantine" => "0",
-			"roomid" => "7",
-			"transactionid" => "4057",
-			"transactionid_original" => "4057"
-		),
-	)
+	'inventory_room' => $ret,
 ));
